1be3215cda5143ef2fd73413d7cdba758f2e0740
Byron Ramírez <87165998+byronrmrz@users.noreply.github.com>
2025-09-22 00:16:43 -0600

    first commit

---
+++ src/main/java/edu/galileo/agenda/model/Contacto.java
@@ -0,0 +1,26 @@
+package edu.galileo.agenda.model;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "Contacto")
+@Data
+public class Contacto {
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column
+    private Integer id;
+    @Column
+    private String nombre;
+    @Column
+    private String direccion;
+    @Column
+    private String telefono;
+
+    
+}
